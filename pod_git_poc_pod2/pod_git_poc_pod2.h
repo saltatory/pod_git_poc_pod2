5286a698560c283f698b85df587f76b83f589fcc
= <=>
2015-10-24 16:44:31 -0700

    Initial Commit

---
+++ pod_git_poc_pod2/pod_git_poc_pod2.h
@@ -0,0 +1,19 @@
+//
+//  pod_git_poc_pod2.h
+//  pod_git_poc_pod2
+//
+//  Created by Jeffrey Hohenstein on 10/24/15.
+//  Copyright © 2015 Jeffrey Hohenstein. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for pod_git_poc_pod2.
+FOUNDATION_EXPORT double pod_git_poc_pod2VersionNumber;
+
+//! Project version string for pod_git_poc_pod2.
+FOUNDATION_EXPORT const unsigned char pod_git_poc_pod2VersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <pod_git_poc_pod2/PublicHeader.h>
+
+
